2139757e90ec2a2397eda67b95a63793c7a4c204
Pavel Sterba <email@pavelsterba.com>
2016-06-29 15:47:18 +0200

    Alpha loader for Tradice added

---
+++ imports/loaders/Tradice.js
@@ -0,0 +1,32 @@
+let cheerio = require("cheerio");
+
+class TradiceLoader {
+    addToMenu(name, price = '') {
+        this.menu.push({
+            food: name,
+            price: price
+        });
+    }
+
+    loadData() {
+        let today = moment().day();
+        let $ = cheerio.load(Meteor.http.get('http://www.tradiceandel.cz/cz/denni-nabidka/').content);
+        let loader = this;
+
+        $('.separator-section').each(function(i, elem) {
+            if (i + 1 === today) {
+                $(this).find('.item').each(function(i, elem) {
+                    let name = $(this).find('div').first().text().trim();
+                    let price = $(this).find('div').last().text().trim();
+                    loader.addToMenu(name, price);
+                });
+            }
+        });
+    }
+}
+
+Tradice = new TradiceLoader();
+Tradice.longitude = 14.401515;
+Tradice.latitude = 50.06924;
+
+export var Tradice;
--- server/main.js
+++ server/main.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Restaurants } from '../imports/api/restaurants.js';
 import { HomeOffice } from '../imports/loaders/HomeOffice.js';
+import { Tradice } from '../imports/loaders/Tradice.js';
 
 Meteor.startup(() => {
     HomeOffice.menu = [];
@@ -17,4 +18,20 @@ Meteor.startup(() => {
     console.log("HomeOffice loaded");
 
     Restaurants.update('DAwHA9tZb97ZTfSkX', restaurant);
+
+
+    Tradice.menu = [];
+    Tradice.loadData();
+    menu = Tradice.menu;
+
+    restaurant = {
+        title: "Tradice",
+        menu: menu,
+        latitude: Tradice.latitude,
+        longitude: Tradice.longitude
+    };
+
+    console.log("Tradice loaded");
+
+    Restaurants.update('kLRwYuWMYWimQMwa6', restaurant);
 });
